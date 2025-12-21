b375c87bdef51759c03f85356c6aafd3aa040466
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-12-21 03:16:06 -0300

    fix: login page route

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,13 +1,16 @@
-import type { Route } from "./+types/home";
+import type { Route } from "./+types/login";
 import LoginPage from "~/pages/Login.page";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Code - Login" },
-    { name: "description", content: "Faça login na plataforma code para usufruir de nossos serviços!" },
+    {
+      name: "description",
+      content: "Faça login na plataforma Code para usufruir de nossos serviços!",
+    },
   ];
 }
 
-export default function Home() {
-  return <LoginPage />
+export default function LoginRoute() {
+  return <LoginPage />;
 }
