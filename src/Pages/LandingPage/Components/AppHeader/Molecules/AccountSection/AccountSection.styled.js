347d6c03972e0090c05520741eaf40bcd21e7584
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-12-21 02:39:25 -0300

    restart project

--- src/Pages/LandingPage/Components/AppHeader/Molecules/AccountSection/AccountSection.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-// account section container
-export const AccountSectionContainer = styled.section`
-    display: flex;
-    gap: 11px;
-`
--- src/Pages/LandingPage/Components/HeroSection/Atoms/Title/CallToActionTitle.jsx
+++
@@ -1,8 +0,0 @@
-import { CallToActionTitleContainer } from "../Title/CallToActionTitle.styled";
-
-export const  CallToActionTitle = () => (
-    <CallToActionTitleContainer>
-        Construa seu projeto <br className="mobile"/> em <br className="desktop"/>
-        <span className="mobile-decoration">30 segundos.</span> <span className="desktop">Não é exagero!</span>
-    </CallToActionTitleContainer>
-);
