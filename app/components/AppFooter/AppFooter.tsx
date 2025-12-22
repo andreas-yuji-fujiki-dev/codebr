2ae8657e7a1fb789f9924922d67515201299ceec
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-12-22 12:19:28 -0300

    feat: footer responsivity

--- app/components/AppFooter/AppFooter.tsx
+++ app/components/AppFooter/AppFooter.tsx
@@ -1,6 +1,6 @@
 // objects
 import { ProjectImagesObject } from "~/json/ProjectImagesObject"
-import { ProjectLinksObject } from "~/json/ProjectLinksObject"
+import { ProjectLinksObject } from "~/json/ProjectLinksObject";
 
 // component function
 export default function AppFooter(){
@@ -8,18 +8,20 @@ export default function AppFooter(){
   const currentYear = String(date.getFullYear());
 
   return (
-    <footer className="flex justify-between items-center py-20 px-10 mt-35 bg-black">
+    <footer className="flex flex-col gap-10 md:flex-wrap md:gap-0 justify-between items-center py-20 px-10 mt-35 bg-black">
       {/* left side */}
       <div>
-        <img 
-          className="w-50" 
-          src={ProjectImagesObject.CodeLogo.src} 
-          alt={ProjectImagesObject.CodeLogo.alt} 
-        />
+        <a href={ProjectLinksObject.Home.path}>
+          <img 
+            className="w-50" 
+            src={ProjectImagesObject.CodeLogo.src} 
+            alt={ProjectImagesObject.CodeLogo.alt} 
+          />
+        </a>
       </div>
 
       {/* right side */}
-      <div className="flex flex-col gap-2">
+      <div className="flex text-center md:text-left flex-col gap-2">
           <span>
             &copy; Code {currentYear} by Obscure Network <br />
           </span>
