9117a4df7cadd1a3f81690cdc1f60ef639495e03
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-12-22 12:32:01 -0300

    feat: animated page components renderization

--- app/pages/Home.page.tsx
+++ app/pages/Home.page.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 // components
 import AppHeader from "~/components/AppHeader/AppHeader";
 import HeroSection from "~/components/HeroSection/HeroSection";
@@ -6,16 +8,57 @@ import ResourcesSection from "~/components/ResourcesSection/ResourcesSection";
 import PricingSection from "~/components/PricingSection/PricingSection";
 import AppFooter from "~/components/AppFooter/AppFooter";
 
-// page function
+// rendering animations
+const sectionVariants = {
+  hidden: {
+    opacity: 0,
+    y: 40,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+// reusable props
+const motionProps = {
+  variants: sectionVariants,
+  initial: "hidden",
+  whileInView: "visible",
+  viewport: {
+    once: false, 
+    margin: "-120px", 
+  },
+  transition: {
+    duration: 0.6,
+    ease: "easeIn" as const,
+  },
+};
+
 export function HomePage() {
   return (
     <div>
       <AppHeader />
-      <HeroSection />
-      <SolutionsSection />
-      <ResourcesSection />
-      <PricingSection />
-      <AppFooter />
+
+      <div className="flex flex-col gap-50">
+        <HeroSection />
+
+        <motion.div {...motionProps}>
+          <SolutionsSection />
+        </motion.div>
+
+        <motion.div {...motionProps}>
+          <ResourcesSection />
+        </motion.div>
+
+        <motion.div {...motionProps}>
+          <PricingSection />
+        </motion.div>
+
+        <motion.div {...motionProps}>
+          <AppFooter />
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
